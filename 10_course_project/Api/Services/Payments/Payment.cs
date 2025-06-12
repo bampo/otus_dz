4a3c8aae6df3c5c8feb58fc9de8cb088253dba84
Viacheslav Potapov <vpmail@gmail.com>
2025-06-12 10:08:52 +0300

    remove old services

--- 10_course_project/Api/Services/Payments/Payment.cs
+++
@@ -1,8 +0,0 @@
-public class Payment
-{
-    public Guid Id { get; set; }
-    public Guid OrderId { get; set; }
-    public decimal Amount { get; set; }
-    public string Status { get; set; } // Pending, Completed, Failed
-    public string? Reason { get; set; }
-}
--- 10_course_project/Api/Services/Payments/PaymentDbContext.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class PaymentDbContext(DbContextOptions<PaymentDbContext> options) : DbContext(options)
-{
-    public DbSet<Payment> Payments { get; set; }
-}
